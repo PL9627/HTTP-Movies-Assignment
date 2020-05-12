b8d0a105b255500bb1e6410784c898b31ad9e7bb
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-12 15:27:09 -0700

    added necessary dependencies and started on update movie route

--- client/src/App.js
+++ client/src/App.js
@@ -35,6 +35,7 @@ const App = () => {
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
       </Route>
+      <Route exact path="/update-movie/:id" />
     </>
   );
 };
